7da9b915f0fdeec60707aa8bf92bff3e98486124
gunadeepraja <85023142+gunadeepraja@users.noreply.github.com>
2021-05-29 19:11:04 +0530

    Update prob1.py

--- prob1.py
+++ prob1.py
@@ -1,5 +1,4 @@
 """
-10 min
 Implement a group_by_owners function that:
     Accepts a dictionary containing the file owner name for each file name.
     Returns a dictionary containing a list of file names for each owner name, in any order.
